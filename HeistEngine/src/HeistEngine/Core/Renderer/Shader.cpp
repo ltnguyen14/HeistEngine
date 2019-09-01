a6fb04227fc0791e0e17f8792edc9e74925dc15b
Lam Nguyen <lamdr96@gmail.com>
2019-09-01 00:56:08 -0400

    Add initial shader abstraction - WIO

---
+++ HeistEngine/src/HeistEngine/Core/Renderer/Shader.cpp
@@ -0,0 +1,14 @@
+#include "hspch.h"
+#include "Shader.h"
+
+namespace Heist {
+
+	Shader::Shader()
+	{
+	}
+
+	Shader::~Shader()
+	{
+	}
+
+}
---
+++ HeistEngine/src/HeistEngine/Core/Renderer/Shader.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Heist {
+
+	struct Shader
+	{
+	public:
+		Shader();
+		~Shader();
+
+	private:
+
+	};
+
+}
